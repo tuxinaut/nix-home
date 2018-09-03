33ed6a0c0a496eb4a6465b8a73f091a82bb6fc1f
Denny Schäfer <denny.schaefer@tuxinaut.de>
2018-09-03 23:04:31 +0200

    [Config] Allow unfree (Dropbox)

--- configuration.nix
+++ configuration.nix
@@ -189,6 +189,10 @@
     uid = 1000;
   };
 
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
