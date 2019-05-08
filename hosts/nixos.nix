c16afe5aee7efdbcc9b7ca0cc04948bbd7353ca9
Denny Schäfer <denny.schaefer@tuxinaut.de>
2019-05-09 00:32:55 +0200

    [Host] Fix imports for host nixos
    Update name for hardware-configuration.nix

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./nixos-hardware-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
