79d4974779068b8e1c8a6c3e601d9102e70f6829
Denny Schäfer <denny.schaefer@tuxinaut.de>
2019-05-21 23:11:14 +0200

    [Dunst] Add inital configuration

--- home.nix
+++ home.nix
@@ -618,6 +618,59 @@ set conceallevel=0 " 0 = Text is shown normally
 
     services.dunst = {
       enable = true;
+      settings = {
+        global = {
+          font = "monospace 12";
+          allow_markup = "yes";
+          format = "<b>%a:</b> %s\n%b";
+          sort = "yes";
+          indicate_hidden = "yes";
+          alignment = "left";
+          bounce_freq = 0;
+          show_age_threshold = 60;
+          word_wrap = "yes";
+          ignore_newline = "no";
+          geometry = "x3-0-0";
+          transparency = 0;
+          idle_threshold = 120;
+          monitor = 0;
+          follow = "keyboard";
+          sticky_history = "yes";
+          line_height = 0;
+          separator_height = 2;
+          padding = 8;
+          horizontal_padding = 8;
+          separator_color = "frame";
+          startup_notification = "true";
+          #dmenu = /usr/bin/dmenu -p dunst:;
+          #browser = /usr/bin/firefox -new-tab;
+        };
+				frame = {
+						width = 0;
+						color = "#000000";
+				};
+				shortcuts = {
+						close = "mod4+m";
+						close_all = "mod4+shift+m";
+						history = "mod4+n";
+						context = "mod4+shift+i";
+				};
+				urgency_low = {
+						background = "#222222";
+						foreground = "#888888";
+						timeout = 10;
+				};
+				urgency_normal = {
+						background = "#285577";
+						foreground = "#ffffff";
+						timeout = 10;
+				};
+				urgency_critical = {
+						background = "#900000";
+						foreground = "#ffffff";
+						timeout = 0;
+				};
+      };
     };
 
     services.network-manager-applet = {
