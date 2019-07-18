0016767bfc1519207d65eaa26d3c23410e8bde30
Denny Schäfer <denny.schaefer@tuxinaut.de>
2019-07-18 22:44:44 +0200

    [Vim] Move plugin sections

--- hosts/nixos-home.nix
+++ hosts/nixos-home.nix
@@ -4,11 +4,54 @@ let
   modifier = "Mod4";
   move = "50px";
 
+  loadPlugin = plugin: ''
+    set rtp^=${plugin.rtp}
+    set rtp+=${plugin.rtp}/after
+  '';
+
+  vim-quick-scope = pkgs.vimUtils.buildVimPlugin {
+    name = "quick-scope";
+    src = pkgs.fetchFromGitHub {
+      owner = "unblevable";
+      repo = "quick-scope";
+      rev= "994576d997a52b4c7828149e9f1325d1c4691ae2";
+      sha256= "0lr27vwv2bzva9s7f9d856vvls10icwli0kwj5v5f1q8y83fa4zd";
+    };
+
+    buildInputs = [ pkgs.zip pkgs.vim ];
+  };
+
   # See https://nixos.wiki/wiki/Vim
   my_vim_configurable = pkgs.vim_configurable.override {
     python = pkgs.python37Full;
   };
 
+  my_vimPlugins = with pkgs.vimPlugins; [
+    iceberg-vim
+    Tabular
+    vim-indent-guides
+    syntastic
+    fugitive
+    nerdtree
+    ctrlp
+    #vim-gnupg
+    vim-airline
+    #SudoEdit.vim
+    vim-multiple-cursors
+    surround
+    editorconfig-vim
+    vim-better-whitespace
+    #Dockerfile.vim
+    #Vim-Jinja2-Syntax
+    neocomplete
+    neosnippet
+    neosnippet-snippets
+    vim-snippets
+    vim-airline-themes
+    molokai
+    tagbar
+    vim-quick-scope
+  ];
 in
 {
   home.keyboard.layout = "de";
@@ -87,6 +130,9 @@ in
       (
         my_vim_configurable.customize {
           name = "vim";
+          vimrcConfig.packages.myVimPackage = {
+            start = my_vimPlugins;
+          };
 
           vimrcConfig.customRC = ''
             ${ (builtins.readFile ../vim/vimrc) }
