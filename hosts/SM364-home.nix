826e2aafd3aa1bf73f4a6230affb4259de825b23
Denny Schäfer <denny.schaefer@smaato.com>
2020-03-12 12:57:28 +0100

    [Work] SSM

--- hosts/SM364-home.nix
+++ hosts/SM364-home.nix
@@ -30,59 +30,10 @@
     controlPersist = "10m";
     extraConfig = "User denny.schaefer";
     matchBlocks = {
-      "*.aws.smaato*.net,ip-*.compute.internal,ec2-*compute*amazonaws*,*.ec2.internal" = {
+      "i-*,mi-*" = {
         matchType = "host";
         user = "syseng";
-        identitiesOnly = true;
-        identityFile = "~/.ssh/blessid-cert.pub";
-        serverAliveInterval = 20;
-        extraOptions = {
-          StrictHostKeyChecking = "no";
-          UserKnownHostsFile = "/dev/null";
-        };
-      };
-      "*.us-east-1.aws.smaatolabs.net,ip-10-168-*.ec2.internal,jenkins-sys.smaatolabs.net,10.168.*" = {
-        matchType = "host";
-        proxyCommand  = "ssh -W %h:%p bastion-test-us-east-1";
-      };
-      "*.us-east-1.aws.smaato.net,ip-10-1-*.ec2.internal,grafana.smaato.net,10.1.*" = {
-        matchType = "host";
-        proxyCommand  = "ssh -W %h:%p bastion-prod-us-east-1";
-      };
-      "*.eu-west-1.aws.smaato.net,ip-10-6-*.compute.internal,10.6.*" = {
-        matchType = "host";
-        proxyCommand  = "ssh -W %h:%p bastion-prod-eu-west-1";
-      };
-      "*.ap-southeast-1.aws.smaato.net,ip-10-4-*.compute.internal,10.4.*" = {
-        matchType = "host";
-        proxyCommand  = "ssh -W %h:%p bastion-prod-ap-southeast-1";
-      };
-      "bastion-*" = {
-        matchType = "host";
-        user = "jump";
-        identitiesOnly = true;
-        identityFile = "~/.ssh/blessid-cert.pub";
-        serverAliveInterval = 20;
-        extraOptions = {
-          StrictHostKeyChecking = "no";
-          UserKnownHostsFile = "/dev/null";
-        };
-      };
-      "bastion-test-us-east-1" = {
-        matchType = "host";
-        hostname = "smaato-vpc.jump.smaatolabs.net";
-      };
-      "bastion-prod-us-east-1" = {
-        matchType = "host";
-        hostname = "vpc-10-1.jump.smaato.net";
-      };
-      "bastion-prod-eu-west-1" = {
-        matchType = "host";
-        hostname = "vpc-10-6.jump.smaato.net";
-      };
-      "bastion-prod-ap-southeast-1" = {
-        matchType = "host";
-        hostname = "vpc-10-4.jump.smaato.net";
+        proxyCommand  = "sh -c \"aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'\"";
       };
     };
   };
