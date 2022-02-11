47bd32f7e1d0b61adc3e9bf8b630a3ed8dc15b3c
Schäfer, Denny <denny.schaefer@tuxinaut.de>
2022-02-11 16:59:57 +0100

    feat: enable nixos automatic upgrading

--- hosts/dell-7400-dschaefer-hh-immowelt.nix
+++ hosts/dell-7400-dschaefer-hh-immowelt.nix
@@ -210,6 +210,9 @@ xdg = {
     config.allowBroken = true;
   };
 
+  # https://nixos.org/manual/nixos/stable/index.html#sec-upgrading-automatic
+  system.autoUpgrade.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
