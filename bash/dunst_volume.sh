d14f0b5f92436b250c319155639c5f48fae4ceb9
Schäfer, Denny <denny.schaefer@tuxinaut.de>
2025-11-25 09:30:30 +0100

    Adjust volume notification for swaync (SwayNotificationCenter)

--- bash/dunst_volume.sh
+++ bash/dunst_volume.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 # Arbitrary but unique message tag
-msgTag="myvolume"
+msgTag="myVolumeChanger"
 
 SINK=$(pactl info | grep "Default Sink" | cut -f3 -d" ")
 volume=$(pactl list sinks | grep -m 1 -A 7 "${SINK}" |  grep '^[[:space:]]Volume:' | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 
 if [[ $volume == 0 || "$mute" == "off" ]]; then
     # Show the sound muted notification
-    dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    notify-send -e -a "changeVolume" -u low -i audio-volume-muted -h string:synchronous:volume-progress "Volume muted"
 else
     # Show the volume notification
-    dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
-    -h int:value:"$volume" "Volume: ${volume}%"
+    notify-send -e -a "changeVolume" -u low -i audio-volume-high -h string:synchronous:volume-progress -h int:value:"$volume" "Volume: ${volume}%"
 fi
 
 exit 0
