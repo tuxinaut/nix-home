2a8be64fc5b3413bb5243a162e418687a9cef99e
Denny Schäfer <denny.schaefer@tuxinaut.de>
2025-12-03 08:30:37 +0100

    [XPS13] Enable Gnome keyring service

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -92,6 +92,8 @@ in
 
   services.udisks2.enable = true;
 
+  services.gnome.gnome-keyring.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.completion.enable = true;
