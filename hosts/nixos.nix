c9673c36e45826666b11208f72d3ea9f39aaefcb
Denny Schäfer <denny.schaefer@tuxinaut.de>
2025-12-03 08:28:33 +0100

    [XPS13] Enable udisk2 service
    Disk management
    
    * https://wiki.ubuntuusers.de/UDisks2/

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -90,6 +90,8 @@ in
     virtualbox
   ];
 
+  services.udisks2.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.completion.enable = true;
