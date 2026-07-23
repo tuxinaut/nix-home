b7185ccf54b3b6d0b0eb626ca5c9b0a23d5c4db7
Schäfer, Denny <denny.schaefer@tuxinaut.de>
2026-07-23 13:49:44 +0200

    chore(LATITUDE7440): code style fixes

--- hosts/dell-7440-dschaefer-hh-immowelt-hardware-configuration.nix
+++ hosts/dell-7440-dschaefer-hh-immowelt-hardware-configuration.nix
@@ -6,7 +6,7 @@
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
@@ -25,12 +25,12 @@
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/1BCB-E9E5";
-      fsType = "vfat";
-    };
+    fsType = "vfat";
+  };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/50f8cd2c-d81b-4870-8d20-3f09ae5da887"; }
-    ];
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
